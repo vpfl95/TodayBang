f2cf5335a34f4fa68df2b7cc12942f52f6873ab6
olrlobt <317tmdgjs@naver.com>
2022-09-19 17:43:43 +0900

    Header mouseover event2

--- src/main/webapp/resources/js/header.js
+++ src/main/webapp/resources/js/header.js
@@ -1,6 +1,6 @@
 const subMenu = document.querySelector("#subMenu");
 const menus = document.getElementsByClassName("menu");
-
+const header = document.querySelector("#header");
 
 
 
@@ -23,86 +23,42 @@ for(mn of menus){
 
         } 
 
-
-
         let li = document.createElement("hr");
         let liAttri = document.createAttribute("id");
         liAttri.value = "hr";
         li.setAttributeNode(liAttri);
         subMenu.before(li);
 
-
-        for(let i = 0 ; i < subMenu.children.length ; ){
-            subMenu.children[i].remove();
-        }
-
         if(event.target.id == "storeMenu"){
 
             let arr = ['스토어 홈', '카테고리', '오늘의딜'];
             let url = ['store/url1', 'store/url2', 'store/url3'];
 
-            if(arr.length != url.length){
-                console.log(" arr url 사이즈 다름");
-                return;
-            }
-
-
-
-            for(let i = 0 ; i < arr.length; i ++){
-                let li = document.createElement("li");
-                let liText = document.createTextNode(arr[i]);
-                let liAttri = document.createAttribute("class");
-                liAttri.value = "submenuList";
-                li.setAttributeNode(liAttri);
-                li.appendChild(liText);
-                subMenu.append(li);
-            }
-            
-
+            profileCategory(event.target , arr , url)
 
         }else if(event.target.id == "roomMenu"){
 
             let arr = ['직방 홈', '아파트', '빌라'];
             let url = ['store/url1', 'store/url2', 'store/url3'];
 
-            if(arr.length != url.length){
-                console.log(" arr url 사이즈 다름");
-                return;
-            }
-
-
-
-            for(let i = 0 ; i < arr.length; i ++){
-                let li = document.createElement("li");
-                let liText = document.createTextNode(arr[i]);
-                let liAttri = document.createAttribute("class");
-                liAttri.value = "submenuList";
-                li.setAttributeNode(liAttri);
-                li.appendChild(liText);
-                subMenu.append(li);
-            }
+           
+            profileCategory(event.target , arr , url)
 
         }else if(event.target.id == "comMenu"){
 
             let arr = ['커뮤니티 홈', '이달의 회원', '내 게시글 보기','사진','집들이','노하우'];
             let url = ['store/url1', 'store/url2', 'store/url3', 'store/url3', 'store/url3', 'store/url3'];
 
-            if(arr.length != url.length){
-                console.log(" arr url 사이즈 다름");
-                return;
-            }
+
+            profileCategory(event.target , arr , url)
+
+        }else if(event.target.id == "serviceMenu"){
+
+            let arr = ['공지사항', 'QnA'];
+            let url = ['store/url1', 'store/url2'];
 
 
-
-            for(let i = 0 ; i < arr.length; i ++){
-                let li = document.createElement("li");
-                let liText = document.createTextNode(arr[i]);
-                let liAttri = document.createAttribute("class");
-                liAttri.value = "submenuList";
-                li.setAttributeNode(liAttri);
-                li.appendChild(liText);
-                subMenu.append(li);
-            }
+            profileCategory(event.target , arr , url)
 
         }
     
@@ -110,7 +66,7 @@ for(mn of menus){
     })
 
 
-    const header = document.querySelector("#header");
+   
 
     header.addEventListener("mouseleave", function(event){
 
@@ -124,3 +80,59 @@ for(mn of menus){
     })
 
 }
+
+
+
+function profileCategory(cate , arr , url){
+
+
+
+    for(let i = 0 ; i < subMenu.children.length ; ){
+        subMenu.children[i].remove();
+    }
+
+    if(arr.length != url.length){
+        console.log(" arr url 사이즈 다름");
+        return;
+    }
+
+
+
+        for(let i = 0 ; i < arr.length; i ++){
+            let li = document.createElement("li");
+            let liText = document.createTextNode(arr[i]);
+            let liAttri = document.createAttribute("class");
+            liAttri.value = "submenuList";
+            li.setAttributeNode(liAttri);
+
+            li.appendChild(liText);
+            subMenu.append(li);
+        }
+
+
+
+}
+
+
+
+subMenu.addEventListener("mouseover",function(event){
+
+  
+
+    const submenuList = document.querySelectorAll(".submenuList");
+
+    for(sub of submenuList){
+        if(event.target == sub){
+            sub.style.color = "#38b9e0";
+
+
+        }else{
+            sub.style.color = "";
+        }
+
+    }
+
+
+
+
+})
