9bf06a789360882dc5edb26577ab889ef20d9302
kkomo11 <djqpsdj@naver.com>
2022-09-20 15:10:31 +0900

    AddDTO

---
+++ src/main/java/com/goodee/home/store/product/Option3DTO.java
@@ -0,0 +1,5 @@
+package com.goodee.home.store.product;
+
+public class Option3DTO {
+
+}
--- src/main/java/com/goodee/home/store/product/ProductDAO.java
+++ src/main/java/com/goodee/home/store/product/ProductDAO.java
@@ -1,5 +1,7 @@
 package com.goodee.home.store.product;
 
+import java.util.List;
+
 import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
@@ -13,5 +15,9 @@ public class ProductDAO {
 	
 	public int setProduct(ProductDTO productDTO) throws Exception {
 		return sqlSession.insert(NAMESPACE+"setProduct", productDTO);
+	}
+	
+	public List<CategoryDTO> getCategory() throws Exception {
+		return sqlSession.selectList(NAMESPACE+"getCategory");
 	}
 }
