cb87b8c35e29fe895d7fd7b8aff3dea38f8a98cb
kkomo11 <djqpsdj@naver.com>
2022-09-21 11:12:01 +0900

    setDTO

--- src/main/java/com/goodee/home/store/product/ProductController.java
+++ src/main/java/com/goodee/home/store/product/ProductController.java
@@ -5,8 +5,12 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.goodee.home.store.exhibition.Ex_ProductDTO;
 
 @Controller
 @RequestMapping("/product/*")
@@ -16,8 +20,17 @@ public class ProductController {
 	private ProductService productService;
 
 	@GetMapping("add")
-	public String setProduct() throws Exception {
+	public String getAddProductPage() throws Exception {
 		return "store/products/addProduct";
+	}
+	
+	@PostMapping("add")
+	public String setProduct(Cate_ProductDTO cate_ProductDTO, Ex_ProductDTO ex_ProductDTO,
+			String[] optionName, String[] optionName1, String[] optionName2,
+			String[] optionPrice, String[] optionPrice1, String[] optionPrice2,
+			MultipartFile[] productDetail, MultipartFile[] productImage) throws Exception {
+		System.out.println("test");
+		return "redirect:./add";
 	}
 	
 	@GetMapping("category")
--- src/main/webapp/resources/js/store/addProduct.js
+++ src/main/webapp/resources/js/store/addProduct.js
@@ -3,79 +3,65 @@ const cate1 = document.querySelector("#cate1");
 const cate2 = document.querySelector("#cate2");
 const cate3 = document.querySelector("#cate3");
 const exhibition = document.querySelector("#exhibition");
+const btnAddProduct = document.querySelector("#btnAddProduct");
 let jsonCategory; 
 let jsonExhibition;
 
-function getCategory1() {
+function getCategory() {
     const xhttp = new XMLHttpRequest();
     xhttp.open("GET", "./category");
     xhttp.send();
     xhttp.onreadystatechange=function() {
         if(this.readyState==4 && this.status==200) {
             jsonCategory = JSON.parse(this.responseText.trim());
-            for(let i=0; jsonCategory.length; i++) {
-                if(jsonCategory[i].tier == 1) {
+            for(c of jsonCategory) {
+                if(c.tier == 1) {
                     let option = document.createElement("option");
                     let option_value = document.createAttribute("value");
-                    let option_contents = document.createTextNode(jsonCategory[i].categoryName);
-                    option_value.value=jsonCategory[i].categoryNum;
+                    let option_contents = document.createTextNode(c.categoryName);
+                    option_value.value=c.categoryNum;
                     option.setAttributeNode(option_value);
                     option.appendChild(option_contents);
                     cate1.append(option);
                 }
             }
-            console.log('cate1 : '+cate1.value);
-            
+
+            getMiddle();
+            getSub();
         }
     }
 }
 
-function getCategory2() {
-    const xhttp = new XMLHttpRequest();
-    xhttp.open("GET", "./category");
-    xhttp.send();
-    xhttp.onreadystatechange=function() {
-        if(this.readyState==4 && this.status==200) {
-            jsonCategory = JSON.parse(this.responseText.trim());
-            for(let i=0; i<jsonCategory.length; i++) {
-                if(jsonCategory[i].categoryRef == cate1.value) {
-                    let option = document.createElement("option");
-                    let option_value = document.createAttribute("value");
-                    let option_class = document.createAttribute("class");
-                    option_class.value = 'child';
-                    let option_contents = document.createTextNode(jsonCategory[i].categoryName);
-                    option_value.value = jsonCategory[i].categoryNum;
-                    option.setAttributeNode(option_value);
-                    option.setAttributeNode(option_class);
-                    option.appendChild(option_contents);
-                    cate2.append(option);
-                }
-            }
+function getMiddle() {
+    for(c of jsonCategory) {
+        if(c.categoryRef == cate1.value) {
+            let option = document.createElement("option");
+            let option_value = document.createAttribute("value");
+            let option_class = document.createAttribute("class");
+            option_class.value = 'child';
+            let option_contents = document.createTextNode(c.categoryName);
+            option_value.value = c.categoryNum;
+            option.setAttributeNode(option_value);
+            option.setAttributeNode(option_class);
+            option.appendChild(option_contents);
+            cate2.append(option);
         }
     }
 }
 
-function getCategory3() {
-    const xhttp = new XMLHttpRequest();
-    xhttp.open("GET", "./category");
-    xhttp.send();
-    xhttp.onreadystatechange=function() {
-        if(this.readyState==4 && this.status==200) {
-            jsonCategory = JSON.parse(this.responseText.trim());
-            for(let i=0; i<jsonCategory.length; i++) {
-                if(jsonCategory[i].categoryRef == cate2.value) {
-                    let option = document.createElement("option");
-                    let option_value = document.createAttribute("value");
-                    let option_class = document.createAttribute("class");
-                    option_class.value = 'child2';
-                    let option_contents = document.createTextNode(jsonCategory[i].categoryName);
-                    option_value.value = jsonCategory[i].categoryNum;
-                    option.setAttributeNode(option_value);
-                    option.setAttributeNode(option_class);
-                    option.appendChild(option_contents);
-                    cate3.append(option);
-                }
-            }
+function getSub() {
+    for(c of jsonCategory) {
+        if(c.categoryRef == cate2.value) {
+            let option = document.createElement("option");
+            let option_value = document.createAttribute("value");
+            let option_class = document.createAttribute("class");
+            option_class.value = 'child2';
+            let option_contents = document.createTextNode(c.categoryName);
+            option_value.value = c.categoryNum;
+            option.setAttributeNode(option_value);
+            option.setAttributeNode(option_class);
+            option.appendChild(option_contents);
+            cate3.append(option);
         }
     }
 }
@@ -89,35 +75,8 @@ cate1.onchange=function() {
     for(let i=0; child2.length;) {
         cate3.removeChild(child2[0]);
     }
-    for(let i=0; i<jsonCategory.length; i++) {
-        if(jsonCategory[i].categoryRef == cate1.value) {
-            let option = document.createElement("option");
-            let option_value = document.createAttribute("value");
-            let option_class = document.createAttribute("class");
-            option_class.value = 'child';
-            let option_contents = document.createTextNode(jsonCategory[i].categoryName);
-            option_value.value = jsonCategory[i].categoryNum;
-            option.setAttributeNode(option_value);
-            option.setAttributeNode(option_class);
-            option.appendChild(option_contents);
-            cate2.append(option);
-        }
-    }
-
-    for(let i=0; i<jsonCategory.length; i++) {
-        if(jsonCategory[i].categoryRef == cate2.value) {
-            let option = document.createElement("option");
-            let option_value = document.createAttribute("value");
-            let option_class = document.createAttribute("class");
-            option_class.value = 'child2';
-            let option_contents = document.createTextNode(jsonCategory[i].categoryName);
-            option_value.value = jsonCategory[i].categoryNum;
-            option.setAttributeNode(option_value);
-            option.setAttributeNode(option_class);
-            option.appendChild(option_contents);
-            cate3.append(option);
-        }
-    }
+    getMiddle();
+    getSub();
 }
 
 cate2.onchange=function(){
@@ -125,20 +84,7 @@ cate2.onchange=function(){
     for(let i=0; child2.length;) {
         cate3.removeChild(child2[0]);
     }
-    for(let i=0; i<jsonCategory.length; i++) {
-        if(jsonCategory[i].categoryRef == cate2.value) {
-            let option = document.createElement("option");
-            let option_value = document.createAttribute("value");
-            let option_class = document.createAttribute("class");
-            option_class.value = 'child2';
-            let option_contents = document.createTextNode(jsonCategory[i].categoryName);
-            option_value.value = jsonCategory[i].categoryNum;
-            option.setAttributeNode(option_value);
-            option.setAttributeNode(option_class);
-            option.appendChild(option_contents);
-            cate3.append(option);
-        }
-    }
+    getSub();
 }
 
 function getExhibition() {
@@ -148,16 +94,21 @@ function getExhibition() {
     xhttp.onreadystatechange=function() {
         if(this.readyState==4 && this.status==200) {
             jsonExhibition = JSON.parse(this.responseText.trim());
-            for(let i=0; jsonExhibition.length; i++) {
+            for(e of jsonExhibition) {
                 let option = document.createElement("option");
                 let option_value = document.createAttribute("value");
-                let option_contents = document.createTextNode(jsonExhibition[i].exName);
-                option_value.value=jsonExhibition[i].exhibitionCode;
+                let option_contents = document.createTextNode(e.exName);
+                option_value.value=e.exhibitionCode;
                 option.setAttributeNode(option_value);
                 option.appendChild(option_contents);
                 exhibition.append(option);
             }
-            
         }
     }
 }
+
+// btnAddProduct.onclick=function() {
+//     console.log('cate1 = ',cate1.value);
+//     console.log('cate2 = ',cate2.value);
+//     console.log('cate3 = ',cate3.value);
+// }
