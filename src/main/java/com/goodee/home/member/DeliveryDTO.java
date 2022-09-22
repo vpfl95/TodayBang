509a55f619cd6a132b942336be66dfaa48f2ea67
olrlobt <317tmdgjs@naver.com>
2022-09-22 09:28:35 +0900

    Add DeliveryDTO

---
+++ src/main/java/com/goodee/home/member/DeliveryDTO.java
@@ -0,0 +1,62 @@
+package com.goodee.home.member;
+
+public class DeliveryDTO {
+
+	
+	private Long addressNum;
+	private String address;
+	private String phone;
+	private String name;
+	private String note;
+	private String deliveryName;
+	private String userId;
+	
+	
+	public Long getAddressNum() {
+		return addressNum;
+	}
+	public void setAddressNum(Long addressNum) {
+		this.addressNum = addressNum;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public String getPhone() {
+		return phone;
+	}
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getNote() {
+		return note;
+	}
+	public void setNote(String note) {
+		this.note = note;
+	}
+	public String getDeliveryName() {
+		return deliveryName;
+	}
+	public void setDeliveryName(String deliveryName) {
+		this.deliveryName = deliveryName;
+	}
+	public String getUserId() {
+		return userId;
+	}
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+	
+	
+	
+	
+	
+}
